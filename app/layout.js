84939270c09f81654dc587038a1b03087b6ca434
tsotne <tsotnemeskhuri@gmail.com>
2024-12-13 19:59:27 +0400

    first commit

---
+++ app/layout.js
@@ -0,0 +1,38 @@
+import Logo from "@/app/_components/Logo";
+import Navigation from "@/app/_components/Navigation";
+import Header from "@/app/_components/Header";
+
+import "@/app/_styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+console.log(josefin);
+
+export const metadata = {
+  title: {
+    template: "%s - The Wild Oasis",
+    default: "Welcome - The Wild Oasis ",
+  },
+  description:
+    "Luxurious cabins hotel, located in the heart of the Italian Dolmites, surrounded by beautiful mountains and dark forests.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${josefin.className} bg-primary-950 antialiased text-primary-100 min-h-screen flex flex-col`}
+      >
+        <Header />
+
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+import bg from "@/public/bg.png";
+
+export default function Page() {
+  return (
+    <main className="mt-24">
+      {/* <img src="/bg.png" alt="Mountains and forests with two cabins" /> */}
+      <Image
+        className="object-cover object-top"
+        src={bg}
+        fill
+        quality={100}
+        placeholder="blur"
+        alt="Mountains and forests with two cabins"
+      />
+
+      <div className="relative z-10 text-center">
+        <h1 className="text-8xl text-primary-50 mb-10 tracking-tight font-normal">
+          Welcome to paradise.
+        </h1>
+        <Link
+          href="/cabins"
+          className="bg-accent-500 px-8 py-6 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
+        >
+          Explore luxury cabins
+        </Link>
+      </div>
+    </main>
+  );
+}
