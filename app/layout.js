4fd74cd30ab86af2e97ff37089ec7098d9d39f96
tsotne <tsotnemeskhuri@gmail.com>
2024-12-15 16:23:50 +0400

    Give Full page height to layout main content

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,3 @@
-import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/Navigation";
 import Header from "@/app/_components/Header";
 
 import "@/app/_styles/globals.css";
@@ -9,8 +7,6 @@ const josefin = Josefin_Sans({
   subsets: ["latin"],
   display: "swap",
 });
-
-console.log(josefin);
 
 export const metadata = {
   title: {
@@ -29,8 +25,8 @@ export default function RootLayout({ children }) {
       >
         <Header />
 
-        <div className="flex-1 px-8 py-12">
-          <main className="max-w-7xl mx-auto">{children}</main>
+        <div className="flex-1 px-8 py-12 flex">
+          <main className="w-full max-w-7xl mx-auto">{children}</main>
         </div>
       </body>
     </html>
