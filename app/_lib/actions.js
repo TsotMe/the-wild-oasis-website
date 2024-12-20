05aac96d268404df7591e6bac275ff23430e7544
tsotne <tsotnemeskhuri@gmail.com>
2024-12-20 16:30:03 +0400

    Update updateProfile server function

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -2,6 +2,24 @@
 
 import { auth, signIn, signOut } from "@/app/_lib/auth";
 import { updateGuest } from "@/app/_lib/data-service";
+import { revalidatePath } from "next/cache";
+
+export async function updateProfile(formData) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const nationalID = formData.get("nationalID");
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+
+  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID))
+    throw new Error("Please provide a valid national ID");
+
+  const updateData = { nationality, countryFlag, nationalID };
+
+  await updateGuest(session.user.guestId, updateData);
+
+  revalidatePath("/account/profile", "page");
+}
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
@@ -9,27 +27,4 @@ export async function signInAction() {
 
 export async function signOutAction() {
   await signOut({ redirectTo: "/" });
-}
-
-export async function updateProfile(formData) {
-  const session = await auth();
-
-  if (session === null) {
-    throw new Error("You must be logged in.");
-  }
-
-  const nationalID = formData.get("nationalID");
-  const [nationality, countryFlag] = formData.get("nationality").split("%");
-
-  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID)) {
-    throw new Error("Please provide a valid national ID");
-  }
-
-  const updateData = {
-    nationality,
-    nationalID: String(nationalID),
-    countryFlag,
-  };
-
-  await updateGuest(session.user.guestId, updateData);
 }
